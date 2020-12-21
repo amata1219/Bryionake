b6da86dbfefd530872a0f83f2867ed69a88c69b4
amata1219 <suzukiamata@gmail.com>
2020-12-21 17:50:13 +0900

    :sparkles: Parserを連結出来るようにし魔界

--- src/amata1219/bryionake/dsl/parser/FailableParser.java
+++ src/amata1219/bryionake/dsl/parser/FailableParser.java
@@ -2,8 +2,14 @@ package amata1219.bryionake.dsl.parser;
 
 import amata1219.bryionake.type.Either;
 
+import java.util.function.Function;
+
 public interface FailableParser<T> {
 
     Either<String, T> tryParse(String arg);
+
+    default <U> FailableParser<U> append(Function<T, Either<String, U>> mapper) {
+        return arg -> tryParse(arg).flatMap(mapper);
+    }
 
 }
